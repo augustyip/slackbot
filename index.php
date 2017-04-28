214b0fce82fb7b639128ebdf6de693fc84cbe063
August Yip <augustyip@gmail.com>
2017-04-28 12:40:24 +0800

    fix coding standard

--- index.php
+++ index.php
@@ -23,31 +23,27 @@ $slack_client->setToken($slack_token);
 $slack_client->on('message', function ($data) use ($slack_client, $gitlab_client, $gitlab_project_id) {
     echo "\n";
 
-    if (!isset($data['bot_id']) && preg_match_all('/#(\d+)/', $data['text'], $matches) !== false){
-      foreach ($matches[1] as $issue_id) {
+    if (!isset($data['bot_id']) && preg_match_all('/#(\d+)/', $data['text'], $matches) !== false) {
+        foreach ($matches[1] as $issue_id) {
+            $issue = $gitlab_client->api('issues')->show($gitlab_project_id, $issue_id);
+            if (!empty($issue)) {
+                $issue = reset($issue);
 
-        $issue = $gitlab_client->api('issues')->show($gitlab_project_id, $issue_id);
-        if (!empty($issue)) {
-          $issue = reset($issue);
-
-          $slack_client->getChannelById($data['channel'])->then(function (\Slack\Channel $channel) use ($slack_client, $issue) {
-
-              $fields = array(
+                $slack_client->getChannelById($data['channel'])->then(function (\Slack\Channel $channel) use ($slack_client, $issue) {
+                    $fields = array(
                 new Slack\Message\AttachmentField('Title1', 'Text', false),
               );
-              $message = $slack_client->getMessageBuilder()
+                    $message = $slack_client->getMessageBuilder()
                 ->setText('Issue *#' . $issue['iid'] . '* from MDM Development / day-one-v-one')
                 ->setChannel($channel)
                 ->addAttachment(new Slack\Message\Attachment('<' . $issue['web_url'] . '|' . $issue['title'] . '>', $issue['description'], null, '36a64f', $fields))
                 ->create();
 
-              $slack_client->postMessage($message);
-              echo "message sent.\n";
-
-          });
-
+                    $slack_client->postMessage($message);
+                    echo "message sent.\n";
+                });
+            }
         }
-      }
     }
 });
 
